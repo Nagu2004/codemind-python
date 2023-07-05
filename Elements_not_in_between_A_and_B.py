0a9401bb035e87a39f6132be106d10cd86dadfe3
Nagu2004 <97611565+Nagu2004@users.noreply.github.com>
2023-07-05 10:07:42 +0530

    n=int(input()) l=list(map(int,input().split())) l1=[] a,b=map(int,input().split()) for i in range(len(l)):     if l[i]<a or l[i]>b:         l1.append(l[i]) if l1==[]:     print(-1) else:     print(*l1)

---
+++ Elements_not_in_between_A_and_B.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=list(map(int,input().split()))
+l1=[]
+a,b=map(int,input().split())
+for i in range(len(l)):
+    if l[i]<a or l[i]>b:
+        l1.append(l[i])
+if l1==[]:
+    print(-1)
+else:
+    print(*l1)
+        
